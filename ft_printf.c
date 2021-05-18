ae83f8736b62a5d15ce9b9c7a93b67f48d6e37d0
Victor Fernandez barrado <vifernan@c1r11s3.42madrid.com>
2021-05-18 12:54:56 +0200

    05_18_21

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vifernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 11:35:11 by vifernan          #+#    #+#             */
-/*   Updated: 2021/05/17 13:43:56 by vifernan         ###   ########.fr       */
+/*   Updated: 2021/05/18 12:54:09 by vifernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,67 @@ char	*ft_char_to_string(char c)
 int	ft_myprint(char *input, va_list args)
 {
 	int 		i;
-//	static char *final;
+	static char *final;
+	char		*temp;
+
 
 	i = 0;
 	while (input[i] != '\0')
 	{
-		if (input[i] == '%' && input[i + 1] == 's')
-			ft_putstr_fd(va_arg(args, char *), 1);
-		if (input[i] == '%' && input[i + 1] == 'c')
-			ft_putstr_fd(ft_char_to_string(va_arg(args, int)), 1);
+		if (input[i] != '%')
+		{
+			if (!final)
+				final = ft_strdup(ft_char_to_string(input[i]));
+			else
+			{
+				temp = ft_strjoin(final, ft_char_to_string(input[i]));
+				free(final);
+				final = temp;
+			}
+		}
+		else
+		{
+			if (input[i] == '%' && input[i + 1] == 's')
+			{
+				if (!final)
+					final = ft_strdup(va_arg(args, char*));
+				else
+				{
+					temp = ft_strjoin(final, va_arg(args, char*));
+					free(final);
+					final = temp;
+				}
+				i++;
+			}
+			if (input[i] == '%' && input[i + 1] == 'c')
+			{
+				if (!final)
+					final = ft_strdup(ft_char_to_string(va_arg(args, int)));
+				else
+				{
+					temp = ft_strjoin(final, ft_char_to_string(va_arg(args, int)));
+					free(final);
+					final = temp;
+				}
+				i++;
+			}
+			if (input[i] == '%' && input[i + 1] == 'd')
+			{
+				if (!final)
+					final = ft_strdup(ft_itoa(va_arg(args, int)));
+				else
+				{
+					temp = ft_strjoin(final, ft_itoa(va_arg(args, int)));
+					free(final);
+					final = temp;
+				}
+				i++;
+			}
+		}
 		i++;
 	}
+	ft_putstr_fd(final, 1);
+	free(final);
 	return (i);
 }
 
@@ -55,11 +105,11 @@ int	ft_printf(const char *input, ...)
 
 int main()
 {
-/*	int a = 23;
+	int a = 23;
 	int b = -1;
-	int c = 0;*/
+	int c = 0;
 	char d = 'V';
 	char *e = "Hola";
 
-	ft_printf("%s %cictor\n", e, d);
+	ft_printf("%d %d %d %s %cict%or          \n\n\n\n\n", a, b, c, e, d);
 }
